c84bc4b20b638a05e4a78c119afe629fb270e40e
nnnsrn <nisrinaizza36@gmail.com>
2025-04-24 21:04:14 +0700

    Update delete.php

--- quickflick/quickflick/delete.php
+++ quickflick/quickflick/delete.php
@@ -1,21 +1,14 @@
 <?php
 include 'db.php';
 
-// Check if 'id' exists in POST data
 if (isset($_POST['id']) && is_numeric($_POST['id'])) {
-    // Get the movie ID from the form
     $movie_id = $_POST['id'];
-
-    // Escape the movie ID to prevent SQL injection
     $movie_id_safe = $conn->real_escape_string($movie_id);
 
-    // Prepare SQL query to delete the movie from the database
     $sql = "DELETE FROM movie WHERE movie_id = $movie_id_safe";
 
-    // Execute the query
     if ($conn->query($sql)) {
         echo "Movie deleted successfully!";
-        // Redirect to the movies page after deletion
         header("Location: index.php");
         exit;
     } else {
