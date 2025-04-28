003e4ee44f57c51cedf11ce6a088a28959ddd495
praisi-ai <praisilia.anastasya@gmail.com>
2025-04-28 17:37:19 +0700

    Update movie_project.sql

--- quickflick/quickflick/movie_project.sql
+++ quickflick/quickflick/movie_project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:8111
--- Generation Time: Apr 28, 2025 at 10:26 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Apr 28, 2025 at 10:36 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `department`
---
-
-CREATE TABLE `department` (
-  `department_id` int(11) NOT NULL,
-  `department_name` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `department`
---
-
-INSERT INTO `department` (`department_id`, `department_name`) VALUES
-(1, 'Directing'),
-(2, 'Writing'),
-(3, 'Production'),
-(4, 'Visual Effects'),
-(5, 'Sound');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `genre`
 --
 
 CREATE TABLE `genre` (
-  `genre_id` int(11) NOT NULL,
-  `genre_name` varchar(50) DEFAULT NULL
+  `genre_id` int NOT NULL,
+  `genre_name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -73,37 +51,41 @@ INSERT INTO `genre` (`genre_id`, `genre_name`) VALUES
 --
 
 CREATE TABLE `movie` (
-  `movie_id` int(11) NOT NULL,
-  `title` varchar(150) DEFAULT NULL,
-  `budget` bigint(20) DEFAULT NULL,
+  `movie_id` int NOT NULL,
+  `title` varchar(150) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `budget` bigint DEFAULT NULL,
   `release_date` date DEFAULT NULL,
-  `revenue` bigint(20) DEFAULT NULL,
-  `runtime` int(11) DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL,
+  `revenue` bigint DEFAULT NULL,
+  `runtime` int DEFAULT NULL,
+  `status` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `votes_avg` decimal(3,1) DEFAULT NULL,
-  `votes_count` int(11) DEFAULT NULL,
-  `poster` varchar(255) DEFAULT NULL
+  `votes_count` int DEFAULT NULL,
+  `poster` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `studio_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `movie`
 --
 
-INSERT INTO `movie` (`movie_id`, `title`, `budget`, `release_date`, `revenue`, `runtime`, `status`, `votes_avg`, `votes_count`, `poster`) VALUES
-(1, 'Avengers: Endgame', 356000000, '2019-04-26', 2797800564, 181, 'Released', 8.4, 700000, 'https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg'),
-(2, 'La La Land', 30000000, '2016-12-09', 446100000, 128, 'Released', 8.0, 500001, 'https://m.media-amazon.com/images/I/41-DDcNrazL._AC_UF894,1000_QL80_.jpg'),
-(3, 'Iron Man', 140000000, '2008-05-02', 585000000, 126, 'Released', 7.9, 600000, 'https://mir-s3-cdn-cf.behance.net/project_modules/source/e9cef893450699.5e65542bcfc77.jpg'),
-(4, 'Jungle Cruise', 200000000, '2021-07-30', 220000000, 127, 'Released', 6.7, 200000, 'https://m.media-amazon.com/images/I/711jcRM+QZL._AC_UF894,1000_QL80_.jpg'),
-(5, 'Spider-Man: Homecoming', 175000000, '2017-07-07', 880000000, 133, 'Released', 7.5, 550000, 'https://m.media-amazon.com/images/I/71HQ7lBgbGL._AC_UF894,1000_QL80_.jpg'),
-(11, 'Harry Potter and the Sorcerer\'s Stone', 125000000, '2001-11-16', 1026428854, 152, 'Released', 7.7, 902000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeQbkD_MyxjNNCInv_A148UO94wS09VRvU7g&s'),
-(12, 'The Lord of the Rings: The Fellowship of the Ring', 93000000, '2001-12-19', 888483037, 178, 'Released', 8.9, 2100000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSozxOlu5feBVSPyRnHNO203Rtx9XhpZuXhtQ&s'),
-(13, 'The Lord of the Rings: The Two Towers', 94000000, '2002-12-18', 938532865, 179, 'Released', 8.8, 1900000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXf53cAHVWrrMRe9stGwbysSTMk8pXjpLnYw&s'),
-(14, 'The Lord of the Rings: The Return of the King', 94000000, '2003-12-17', 1138585992, 211, 'Released', 9.0, 2100000, 'https://m.media-amazon.com/images/M/MV5BMTZkMjBjNWMtZGI5OC00MGU0LTk4ZTItODg2NWM3NTVmNWQ4XkEyXkFqcGc@._V1_.jpg'),
-(15, 'The Hobbit: An Unexpected Journey', 180000000, '2012-12-14', 1017107150, 169, 'Released', 7.8, 902000, 'https://m.media-amazon.com/images/I/61n3qIANJ5L.jpg'),
-(16, 'The Hobbit: The Desolation of Smaug', 225000000, '2013-12-13', 959079095, 161, 'Released', 7.8, 731000, 'https://m.media-amazon.com/images/I/71iOSKkZvcL._AC_UF894,1000_QL80_.jpg'),
-(17, 'The Hobbit: The Battle of the Five Armies', 250000000, '2014-12-17', 962253946, 144, 'Released', 7.4, 596000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKeUa3PTduLx2Q4H9-mGw8J9n9oFjPCOobyw&s'),
-(18, 'Spirited Away', 19000000, '2001-07-20', 358900157, 124, 'Released', 8.6, 907000, 'https://s3.amazonaws.com/nightjarprod/content/uploads/sites/249/2024/05/13153104/39wmItIWsg5sZMyRUHLkWBcuVCM-1-scaled.jpg'),
-(19, 'Coco', 175000000, '2017-11-22', 814641172, 105, 'Released', 8.4, 648000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm5Xg3MeEBBBizVNRGPOqRXepysq-OERp6pQ&s');
+INSERT INTO `movie` (`movie_id`, `title`, `budget`, `release_date`, `revenue`, `runtime`, `status`, `votes_avg`, `votes_count`, `poster`, `studio_id`) VALUES
+(1, 'Avengers: Endgame', 356000000, '2019-04-26', 2797800564, 181, 'Released', '8.4', 700000, 'https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg', NULL),
+(2, 'La La Land', 30000000, '2016-12-09', 446100000, 128, 'Released', '8.0', 500001, 'https://m.media-amazon.com/images/I/41-DDcNrazL._AC_UF894,1000_QL80_.jpg', NULL),
+(3, 'Iron Man', 140000000, '2008-05-02', 585000000, 126, 'Released', '7.9', 600000, 'https://mir-s3-cdn-cf.behance.net/project_modules/source/e9cef893450699.5e65542bcfc77.jpg', NULL),
+(4, 'Jungle Cruise', 200000000, '2021-07-30', 220000000, 127, 'Released', '6.7', 200000, 'https://m.media-amazon.com/images/I/711jcRM+QZL._AC_UF894,1000_QL80_.jpg', NULL),
+(5, 'Spider-Man: Homecoming', 175000000, '2017-07-07', 880000000, 133, 'Released', '7.5', 550000, 'https://m.media-amazon.com/images/I/71HQ7lBgbGL._AC_UF894,1000_QL80_.jpg', NULL),
+(11, 'Harry Potter and the Sorcerer\'s Stone', 125000000, '2001-11-16', 1026428854, 152, 'Released', '7.7', 902000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeQbkD_MyxjNNCInv_A148UO94wS09VRvU7g&s', NULL),
+(12, 'The Lord of the Rings: The Fellowship of the Ring', 93000000, '2001-12-19', 888483037, 178, 'Released', '8.9', 2100000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSozxOlu5feBVSPyRnHNO203Rtx9XhpZuXhtQ&s', NULL),
+(13, 'The Lord of the Rings: The Two Towers', 94000000, '2002-12-18', 938532865, 179, 'Released', '8.8', 1900000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXf53cAHVWrrMRe9stGwbysSTMk8pXjpLnYw&s', NULL),
+(14, 'The Lord of the Rings: The Return of the King', 94000000, '2003-12-17', 1138585992, 211, 'Released', '9.0', 2100000, 'https://m.media-amazon.com/images/M/MV5BMTZkMjBjNWMtZGI5OC00MGU0LTk4ZTItODg2NWM3NTVmNWQ4XkEyXkFqcGc@._V1_.jpg', NULL),
+(15, 'The Hobbit: An Unexpected Journey', 180000000, '2012-12-14', 1017107150, 169, 'Released', '7.8', 902000, 'https://m.media-amazon.com/images/I/61n3qIANJ5L.jpg', NULL),
+(16, 'The Hobbit: The Desolation of Smaug', 225000000, '2013-12-13', 959079095, 161, 'Released', '7.8', 731000, 'https://m.media-amazon.com/images/I/71iOSKkZvcL._AC_UF894,1000_QL80_.jpg', NULL),
+(17, 'The Hobbit: The Battle of the Five Armies', 250000000, '2014-12-17', 962253946, 144, 'Released', '7.4', 596000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKeUa3PTduLx2Q4H9-mGw8J9n9oFjPCOobyw&s', NULL),
+(18, 'Spirited Away', 19000000, '2001-07-20', 358900157, 124, 'Released', '8.6', 907000, 'https://s3.amazonaws.com/nightjarprod/content/uploads/sites/249/2024/05/13153104/39wmItIWsg5sZMyRUHLkWBcuVCM-1-scaled.jpg', NULL),
+(19, 'Coco', 175000000, '2017-11-22', 814641172, 105, 'Released', '8.4', 648000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm5Xg3MeEBBBizVNRGPOqRXepysq-OERp6pQ&s', NULL),
+(20, 'Enchanted', 80000000, '2007-11-21', 340500000, 107, '0', '7.1', 223690, 'uploads/poster_680f427ebe86b5.52957669.jpg', NULL),
+(21, 'Up', 175000000, '2009-05-29', 735100000, 96, '0', '8.3', 1186000, 'uploads/poster_680f433f8f4511.39756408.jpg', NULL),
+(22, 'Soul', 150000000, '2020-12-25', 121000000, 100, '0', '8.0', 396000, 'uploads/poster_680f4439620f35.34108691.jpg', NULL);
 
 -- --------------------------------------------------------
 
@@ -112,9 +94,9 @@ INSERT INTO `movie` (`movie_id`, `title`, `budget`, `release_date`, `revenue`, `
 --
 
 CREATE TABLE `movie_cast` (
-  `movie_id` int(11) NOT NULL,
-  `person_id` int(11) NOT NULL,
-  `character_name` varchar(100) DEFAULT NULL
+  `movie_id` int NOT NULL,
+  `person_id` int NOT NULL,
+  `character_name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -131,58 +113,12 @@ INSERT INTO `movie_cast` (`movie_id`, `person_id`, `character_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `movie_company`
---
-
-CREATE TABLE `movie_company` (
-  `movie_id` int(11) NOT NULL,
-  `company_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `movie_company`
---
-
-INSERT INTO `movie_company` (`movie_id`, `company_id`) VALUES
-(1, 1),
-(2, 5),
-(3, 1),
-(4, 4),
-(5, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `movie_crew`
---
-
-CREATE TABLE `movie_crew` (
-  `movie_id` int(11) NOT NULL,
-  `person_id` int(11) NOT NULL,
-  `department_id` int(11) NOT NULL,
-  `job` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `movie_crew`
---
-
-INSERT INTO `movie_crew` (`movie_id`, `person_id`, `department_id`, `job`) VALUES
-(1, 2, 2, 'Writer'),
-(1, 3, 1, 'Director'),
-(2, 4, 1, 'Director'),
-(3, 3, 1, 'Director'),
-(5, 5, 4, 'VFX Artist');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `movie_genre`
 --
 
 CREATE TABLE `movie_genre` (
-  `movie_id` int(11) NOT NULL,
-  `genre_id` int(11) NOT NULL
+  `movie_id` int NOT NULL,
+  `genre_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -191,15 +127,15 @@ CREATE TABLE `movie_genre` (
 
 INSERT INTO `movie_genre` (`movie_id`, `genre_id`) VALUES
 (1, 1),
-(1, 5),
-(2, 2),
-(2, 3),
 (3, 1),
-(3, 5),
-(4, 2),
-(4, 4),
 (5, 1),
-(5, 4);
+(2, 2),
+(4, 2),
+(2, 3),
+(4, 4),
+(5, 4),
+(1, 5),
+(3, 5);
 
 -- --------------------------------------------------------
 
@@ -208,8 +144,8 @@ INSERT INTO `movie_genre` (`movie_id`, `genre_id`) VALUES
 --
 
 CREATE TABLE `person` (
-  `person_id` int(11) NOT NULL,
-  `person_name` varchar(100) DEFAULT NULL
+  `person_id` int NOT NULL,
+  `person_name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -226,48 +162,43 @@ INSERT INTO `person` (`person_id`, `person_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `production_company`
---
-
-CREATE TABLE `production_company` (
-  `company_id` int(11) NOT NULL,
-  `company_name` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `production_company`
---
-
-INSERT INTO `production_company` (`company_id`, `company_name`) VALUES
-(5, 'Disney'),
-(1, 'Marvel Studios'),
-(2, 'Pixar'),
-(4, 'Universal Pictures'),
-(3, 'Warner Bros');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `review`
 --
 
 CREATE TABLE `review` (
-  `review_id` int(11) NOT NULL,
-  `movie_id` int(11) NOT NULL,
-  `rating` int(11) NOT NULL,
-  `comment` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `review_id` int NOT NULL,
+  `movie_id` int NOT NULL,
+  `rating` int NOT NULL,
+  `comment` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studio`
+--
+
+CREATE TABLE `studio` (
+  `studio_id` int NOT NULL,
+  `studio_name` varchar(255) NOT NULL,
+  `studio_location` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `studio`
+--
+
+INSERT INTO `studio` (`studio_id`, `studio_name`, `studio_location`) VALUES
+(1, 'Marvel Studios', 'United States'),
+(2, 'Pixar', 'United States'),
+(3, 'Warner Bros', 'United States'),
+(4, 'Universal Pictures', 'United States'),
+(5, 'Disney', 'United States');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`department_id`);
 
 --
 -- Indexes for table `genre`
@@ -289,21 +220,6 @@ ALTER TABLE `movie_cast`
   ADD KEY `person_id` (`person_id`);
 
 --
--- Indexes for table `movie_company`
---
-ALTER TABLE `movie_company`
-  ADD PRIMARY KEY (`movie_id`,`company_id`),
-  ADD KEY `company_id` (`company_id`);
-
---
--- Indexes for table `movie_crew`
---
-ALTER TABLE `movie_crew`
-  ADD PRIMARY KEY (`movie_id`,`person_id`,`department_id`),
-  ADD KEY `person_id` (`person_id`),
-  ADD KEY `department_id` (`department_id`);
-
---
 -- Indexes for table `movie_genre`
 --
 ALTER TABLE `movie_genre`
@@ -317,13 +233,6 @@ ALTER TABLE `person`
   ADD PRIMARY KEY (`person_id`);
 
 --
--- Indexes for table `production_company`
---
-ALTER TABLE `production_company`
-  ADD PRIMARY KEY (`company_id`),
-  ADD UNIQUE KEY `company_name` (`company_name`);
-
---
 -- Indexes for table `review`
 --
 ALTER TABLE `review`
@@ -331,44 +240,44 @@ ALTER TABLE `review`
   ADD KEY `movie_id` (`movie_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `studio`
 --
+ALTER TABLE `studio`
+  ADD PRIMARY KEY (`studio_id`);
 
 --
--- AUTO_INCREMENT for table `department`
+-- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `department`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `genre_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `movie_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `person`
 --
 ALTER TABLE `person`
-  MODIFY `person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `production_company`
---
-ALTER TABLE `production_company`
-  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `person_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `review_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `studio`
+--
+ALTER TABLE `studio`
+  MODIFY `studio_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -380,21 +289,6 @@ ALTER TABLE `review`
 ALTER TABLE `movie_cast`
   ADD CONSTRAINT `movie_cast_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
   ADD CONSTRAINT `movie_cast_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
-
---
--- Constraints for table `movie_company`
---
-ALTER TABLE `movie_company`
-  ADD CONSTRAINT `movie_company_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
-  ADD CONSTRAINT `movie_company_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `production_company` (`company_id`);
-
---
--- Constraints for table `movie_crew`
---
-ALTER TABLE `movie_crew`
-  ADD CONSTRAINT `movie_crew_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
-  ADD CONSTRAINT `movie_crew_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
-  ADD CONSTRAINT `movie_crew_ibfk_3` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`);
 
 --
 -- Constraints for table `movie_genre`
@@ -413,3 +307,4 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
